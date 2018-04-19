1765f5a8b1ec57059f97ef257b63285d0791c7b6
Thiago Soares <thiago.soares.jr@gmail.com>
2018-04-19 11:13:37 -0300

    Merge coom versão do nelio alves do final do primeiro capitulo

--- src/main/java/com/nelioalves/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/ProdutoRepository.java
@@ -1,10 +1,9 @@
-package com.blackdog.bootcommerce.repository;
+package com.nelioalves.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.blackdog.bootcommerce.model.Categoria;
-import com.blackdog.bootcommerce.model.Produto;
+import com.nelioalves.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
