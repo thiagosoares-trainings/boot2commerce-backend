0ef83587cd89c745cd267038b0dee149971b4022
thiago soares <thiago.soares.jr@gmail.com>
2018-05-06 11:25:55 -0300

    Teste Boot com MockSMTP

--- src/main/java/com/nelioalves/cursomc/config/ProdConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/ProdConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import com.nelioalves.cursomc.services.mail.EmailService;
+import com.nelioalves.cursomc.services.mail.MockEmailService;
 import com.nelioalves.cursomc.services.mail.SmtpEmailService;
 import com.nelioalves.cursomc.services.test.InitDBService;
 
@@ -32,7 +33,8 @@ public class ProdConfig {
   
   @Bean
   public EmailService emailService() {
-      return new SmtpEmailService();
+    return new MockEmailService();
+      //return new SmtpEmailService(); 
   }
   
 }
