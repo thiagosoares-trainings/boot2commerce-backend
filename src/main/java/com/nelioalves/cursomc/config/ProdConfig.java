b486bdbb087cc5da3363dbb99db2f5d22755f68d
thiago soares <thiago.soares.jr@gmail.com>
2018-05-06 11:17:23 -0300

    Ajustes do Bean de SMTP

---
+++ src/main/java/com/nelioalves/cursomc/config/ProdConfig.java
@@ -0,0 +1,38 @@
+package com.nelioalves.cursomc.config;
+
+import java.text.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import com.nelioalves.cursomc.services.mail.EmailService;
+import com.nelioalves.cursomc.services.mail.SmtpEmailService;
+import com.nelioalves.cursomc.services.test.InitDBService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+  @Autowired
+  private InitDBService dbService;
+  
+  @Value("${spring.jpa.hibernate.ddl-auto}")
+  private String ddlauto;
+  
+  @Bean
+  public boolean instaciateDatabase() throws ParseException {
+    
+    if(ddlauto != null && (ddlauto.equals("create") || ddlauto.equals("create-drop") )) {
+      dbService.initializeTestDataBase();
+    }
+    
+    return true;
+  }
+  
+  @Bean
+  public EmailService emailService() {
+      return new SmtpEmailService();
+  }
+  
+}
