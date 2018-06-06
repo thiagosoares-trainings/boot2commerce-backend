a6fcf9cb372dc074fb9884b9b6aaa495f42d39df
thiago soares <thiago.soares.jr@gmail.com>
2018-06-06 13:10:18 -0300

    ProdConfig

--- src/main/java/com/nelioalves/cursomc/config/ProdConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/ProdConfig.java
@@ -33,8 +33,8 @@ public class ProdConfig {
   
   @Bean
   public EmailService emailService() {
-    return new MockEmailService();
-      //return new SmtpEmailService(); 
+    //return new MockEmailService();
+    return new SmtpEmailService(); 
   }
   
 }
